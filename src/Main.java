f5908872ec76c875955cae1f09741c000407ff4f
arnavgullia <arnavgullia.eagle@gmail.com>
2026-03-16 16:04:51 +0530

    UC13

--- src/Main.java
+++ src/Main.java
@@ -1,68 +1,62 @@
-import java.util.*;
-
-interface PalindromeStrategy {
-    boolean isPalindrome(String str);
-}
-
-class StackStrategy implements PalindromeStrategy {
-
-    public boolean isPalindrome(String str) {
-        Stack<Character> stack = new Stack<>();
-
-        for (char c : str.toCharArray())
-            stack.push(c);
-
-        for (char c : str.toCharArray()) {
-            if (c != stack.pop())
-                return false;
-        }
-
-        return true;
-    }
-}
-
-class DequeStrategy implements PalindromeStrategy {
-
-    public boolean isPalindrome(String str) {
-        Deque<Character> deque = new ArrayDeque<>();
-
-        for (char c : str.toCharArray())
-            deque.add(c);
-
-        while (deque.size() > 1) {
-            if (deque.removeFirst() != deque.removeLast())
-                return false;
-        }
-
-        return true;
-    }
-}
+import java.util.Scanner;
 
 public class PalindromeCheckerApp {
 
+    public static boolean checkUsingReverse(String str) {
+        String reversed = new StringBuilder(str).reverse().toString();
+        return str.equals(reversed);
+    }
+
+    public static boolean checkUsingTwoPointer(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static boolean checkUsingLoop(String str) {
+        String reversed = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+
+        return str.equals(reversed);
+    }
+
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
 
-        System.out.print("Enter string: ");
-        String input = sc.nextLine();
+        long start1 = System.nanoTime();
+        boolean result1 = checkUsingReverse(input);
+        long end1 = System.nanoTime();
+        long time1 = end1 - start1;
 
-        System.out.println("Choose Strategy:");
-        System.out.println("1. Stack Strategy");
-        System.out.println("2. Deque Strategy");
+        long start2 = System.nanoTime();
+        boolean result2 = checkUsingTwoPointer(input);
+        long end2 = System.nanoTime();
+        long time2 = end2 - start2;
 
-        int choice = sc.nextInt();
+        long start3 = System.nanoTime();
+        boolean result3 = checkUsingLoop(input);
+        long end3 = System.nanoTime();
+        long time3 = end3 - start3;
 
-        PalindromeStrategy strategy;
+        System.out.println("\nResults:");
+        System.out.println("Reverse Method: " + result1 + " | Time: " + time1 + " ns");
+        System.out.println("Two Pointer Method: " + result2 + " | Time: " + time2 + " ns");
+        System.out.println("Loop Reverse Method: " + result3 + " | Time: " + time3 + " ns");
 
-        if (choice == 1)
-            strategy = new StackStrategy();
-        else
-            strategy = new DequeStrategy();
-
-        if (strategy.isPalindrome(input))
-            System.out.println("Palindrome");
-        else
-            System.out.println("Not Palindrome");
+        scanner.close();
     }
 }
